93c325c3c6dad4df6dd34a663b4bb75a46ee2900
lopezkarylle <dr.lopezkarylle@gmail.com>
2023-12-09 23:13:37 +0800

    changed to pink

--- FinalProject/FinalProject.Android/obj/Debug/designtime/Resource.designer.cs
+++ FinalProject/FinalProject.Android/obj/Debug/designtime/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace FinalProject.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.1.111")]
 	public partial class Resource
 	{
 		
